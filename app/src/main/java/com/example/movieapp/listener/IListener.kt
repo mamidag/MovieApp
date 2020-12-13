2a8f2d627d40bdcad0a93ad9155d862718f46f11
MamiDag <mamidag6@gmail.com>
2020-12-14 00:03:22 +0300

    Update IListener.kt

--- app/src/main/java/com/example/movieapp/listener/IListener.kt
+++ app/src/main/java/com/example/movieapp/listener/IListener.kt
@@ -3,6 +3,8 @@ package com.example.movieapp.listener
 import com.example.movieapp.api.model.genres.Genre
 import com.example.movieapp.api.model.movie.Results
 
-interface IListener<T> {
+interface IListener {
     fun onClick(item: Results)
+    fun onClick(item: Genre)
+
 }
