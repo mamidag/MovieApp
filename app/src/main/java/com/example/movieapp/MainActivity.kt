a88e3e9517e4df6136783ffdac657e42e5b19342
MamiDag <mamidag6@gmail.com>
2020-12-17 09:56:19 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -2,11 +2,10 @@ package com.example.movieapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.movieapp.api.model.genres.Genre
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 
-@ExperimentalCoroutinesApi
+
+
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
